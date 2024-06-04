f0d76bacbcf239c30db5169daa263965d2cf5888
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-06-04 12:31:27 +0530

    Experience section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <h2 className="portfolio-logo text-2xl">kp97<span className="logo-dot">.</span></h2>
+        <h2 className="portfolio-logo text-2xl">kp97<span className="logo-dot text-purple-800">.</span></h2>
       </div>
       <div className="m-8 flex items-center gap-4 text-2xl">
         <FaLinkedin/>
