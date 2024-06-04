49914893b14076db3c53fff236d30fcc115d8767
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-06-04 10:33:46 +0530

    About section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,14 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mb-16 lg:text-8xl">K Praveen Kumar</h1>
+            <h1 className="text-6xl font-thin tracking-tight lg:mb-16 lg:text-8xl">K Praveen Kumar</h1>
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Front-end Developer</span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
             <div className="flex justify-center">
-                <img src={dp} alt="Display Pic" />
+                <img className="rounded-2xl" src={dp} alt="Display Pic" width={500} height={500} />
             </div>
         </div>
       </div>
