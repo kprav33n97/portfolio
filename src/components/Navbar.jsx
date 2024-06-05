8332b66c9d31ccc43dffa38a7356743a584a1df2
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-06-05 20:58:33 +0530

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { FaLinkedin} from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
 
@@ -8,13 +8,14 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <h2 className="portfolio-logo text-2xl">kp97<span className="logo-dot text-purple-800">.</span></h2>
+        <h2 className="portfolio-logo text-2xl">kp97<span className="logo-dot text-purple-800 custom-logo">.</span></h2>
       </div>
       <div className="m-8 flex items-center gap-4 text-2xl">
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaFacebook />
-        <FaInstagram/>
+        <a href="https://www.linkedin.com/in/kprav33n97/" target="_blank"><FaLinkedin /></a>
+        <a href="https://github.com/kprav33n97" target="_blank"><FaGithub/></a>
+        <a href="mailto:kpraveenkumar2006@gmail.com" target="_blank"><FaEnvelope />
+</a>
+        <a href="https://www.instagram.com/kprav33n97/" target="_blank"><FaInstagram/></a>     
       </div>
     </nav>
   )
